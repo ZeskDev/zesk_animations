30c83cead5c5040357dd07eebf25513e7c859665
ZeskDev <91477035+ZeskDev@users.noreply.github.com>
2021-09-27 19:05:54 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,3 +2,13 @@ Config = {}
 Locales = {}
 
 Config.Locale = "es" --Available locales: en, es, eus
+
+Locales = {
+   ['es'] = {
+       ['animation_menu_name'] = "Animaciones",
+       ['ragdoll_animation'] = "Tirarse al Suelo",
+       ['crosshands_animation'] = "Cruzar Brazos",
+       ['smoke_animation'] = "Fumar",
+       ['handsup_animation'] = "Manos Arriba"
+   }
+}
