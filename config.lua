d8e87a38ec12fb67debd1199b505982715e7b67c
ZeskDev <91477035+ZeskDev@users.noreply.github.com>
2021-09-27 21:34:38 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -14,6 +14,11 @@ Locales = {
        ['ragdoll_animation_error_car'] = "No puedes Hacer esto en un vehiculo",
       ['radio_anim'] = "Radio",
       ['holster_anim'] = "Pistolera",
-      ['walkstyle1'] = "Caminar 1"
+      ['walkstyle1'] = "Caminar 1",
+      ['walkstyle2'] = "Caminar 2",
+      ['walkstyle3'] = "Caminar 3",
+      ['walkstyle4'] = "Caminar 4",
+      ['walkstyle5'] = "Caminar 5",
+      ['walkstyle6'] = "Caminar 6"
    }
 }
